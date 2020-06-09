47e2fb90f02c38580045f862c2eb4e825ead3cab
DJ Chemik <dj-chemik@wp.pl>
2020-06-09 22:23:20 +0200

    Add empty Spring project as a REST API in backend

---
+++ spring-rest-api/src/main/java/pl/chemik/tournamentmanagerapi/TournamentManagerApiApplication.java
@@ -0,0 +1,13 @@
+package pl.chemik.tournamentmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentManagerApiApplication.class, args);
+	}
+
+}
---
+++ spring-rest-api/src/test/java/pl/chemik/tournamentmanagerapi/TournamentManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.chemik.tournamentmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
