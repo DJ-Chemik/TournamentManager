424971bd9c39b1a2995625d8222946ae94429ebc
DJ Chemik <dj-chemik@wp.pl>
2020-06-10 00:33:50 +0200

    Add url and properties to connect PostgreSQL

--- spring-rest-api/src/main/java/pl/chemik/tournamentmanagerapi/TournamentManagerApplication.java
+++ spring-rest-api/src/main/java/pl/chemik/tournamentmanagerapi/TournamentManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TournamentManagerApiApplication {
+public class TournamentManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TournamentManagerApiApplication.class, args);
+		SpringApplication.run(TournamentManagerApplication.class, args);
 	}
 
 }
