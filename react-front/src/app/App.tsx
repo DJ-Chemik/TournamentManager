1946ed7b0d619a2754752be74cff268348e7f5f8
DJ Chemik <dj-chemik@wp.pl>
2020-06-18 00:19:33 +0200

    Fetch data from Spring REST API

--- react-front/src/app/App.tsx
+++ react-front/src/app/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import LoginPage from './logging/LoginPage';
+import MainPage from './mainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
       <LoginPage/>
+      <br/>
+      <MainPage/>
     </div>
   );
 }
--- react-front/src/app/mainPage/MainPage.tsx
+++ react-front/src/app/mainPage/MainPage.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MainPage = () => {
+  const [tournaments, setTournaments] = useState();
+
+  useEffect( () => {
+    fetch( "http://localhost:8080/api/tournaments")
+      .then(response => response.json())
+      .then(jsonData => {
+        console.log(jsonData);
+        setTournaments(jsonData.results)
+      })
+      .catch( () => console.log("ERORR"))
+    console.log(tournaments);
+  })
+
   return (
     <div>
       
