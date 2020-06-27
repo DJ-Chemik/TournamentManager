853a13abf441ae1e1a0dd0eb6aa106abded00fa7
DJ-Chemik <dj-chemik@wp.pl>
2020-06-27 22:12:46 +0200

    Add pagination - display only 10 tournaments in list

--- react-front/src/app/mainPage/TournamentTilesList.tsx
+++ react-front/src/app/mainPage/TournamentTilesList.tsx
@@ -10,11 +10,54 @@ interface Props {
 
 const TournamentTilesList = ({tournaments, pagination, goToOneTournamentInformation} : Props) => {
   const [tournamentTiles, setTournamentTiles] = useState<any>();
+  const [amoutPages, setAmountPages] = useState<number>(1);
+  const [actualPage, setActualPage] = useState<number>(1);
   
+  // const initializeListFromProps = React.useCallback( () => {
+  //   if(tournaments){
+  //     const tenTournaments = [];
+  //     let firstIndex = actualPage * 10 - 10;
+  //     let lastIndex = actualPage * 10 - 1;
+  //     for (let i = firstIndex; i <= lastIndex; i++) {
+  //       tenTournaments.push(tournaments[i]);        
+  //     }
+      
+  //     return(
+  //       tenTournaments.map( (tournament: ITournament) => {
+  //           return(
+  //             <ul key={tournament.id}>
+  //               <TournamentTile
+  //                 name={tournament.name}
+  //                 discipline={tournament.discipline}
+  //                 date={tournament.time}
+  //                 onClick={()=>{ goToOneTournamentInformation(tournament)}}
+  //               />
+  //             </ul>
+  //           )
+  //         }
+  //       )
+  //     );
+  //   }
+  //   return null;
+  // },[goToOneTournamentInformation, tournaments])
+
   const initializeListFromProps = React.useCallback( () => {
     if(tournaments){
+      const tenTournaments: ITournament[] = [];
+      let firstIndex = actualPage * 10 - 10;
+      let lastIndex = actualPage * 10 - 1;
+      for (let i = firstIndex; i <= lastIndex; i++) {
+        if(tournaments[i]){
+          tenTournaments.push(tournaments[i]);        
+        }else{
+          break;
+        }
+      }
+      console.log(tenTournaments);
+      
+      
       return(
-        tournaments.map( (tournament: ITournament) => {
+        tenTournaments.map( (tournament: ITournament) => {
             return(
               <ul key={tournament.id}>
                 <TournamentTile
@@ -30,18 +73,41 @@ const TournamentTilesList = ({tournaments, pagination, goToOneTournamentInformat
       );
     }
     return null;
-  },[goToOneTournamentInformation, tournaments])
+  },[goToOneTournamentInformation, tournaments, actualPage])
 
   useEffect( () => {
+    if(tournaments?.length){
+      setAmountPages(tournaments?.length / 10)
+    }
     const tmp = initializeListFromProps();
     setTournamentTiles(tmp);
   }, [tournaments, initializeListFromProps]);
   
+  useEffect(()=>{
+    const tmp = initializeListFromProps();
+    setTournamentTiles(tmp);
+  }, [actualPage])
   
+  const handleClickPreviousPage = () => {
+    if(actualPage>1){
+      setActualPage(actualPage - 1);
+    }
+  }
+
+  const handleClickNextPage = () => {
+    if(actualPage<amoutPages){
+      setActualPage(actualPage + 1);
+    }
+  }
   
   return (
     <div>
         {tournamentTiles}
+        <div>
+          <button onClick={handleClickPreviousPage}>Poprzednia strona</button>
+          Aktualna strona: {actualPage}
+          <button onClick={handleClickNextPage}>Następna strona</button>
+        </div>
     </div>
   );
 }
